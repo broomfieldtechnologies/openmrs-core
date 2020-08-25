8fc3dced6ee40758a6cc091a72c4bc0c93d1e900
varsha <varsha@varshas-MacBook-Pro.local>
2020-08-25 11:13:08 +0530

    US10032 changes

--- api/src/main/java/org/openmrs/Location.java
+++ api/src/main/java/org/openmrs/Location.java
@@ -287,7 +287,7 @@ public class Location extends BaseCustomizableMetadata<LocationAttribute> implem
 	@Override
 	public List<Location> getPossibleValues() {
 		try {
-			return Context.getLocationService().getAllLocations();
+			return Context.getLocationService().getAllLocationsForEnterpriseId();
 		}
 		catch (Exception e) {
 			return Collections.emptyList();
